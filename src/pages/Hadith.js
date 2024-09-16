895c204ecc46a9197c0c662693a5381eaa6a4399
Asif khan <khanasif0870@gmail.com>
2024-09-16 23:21:39 +0530

    adding hadith books

--- src/pages/Hadith.js
+++ src/pages/Hadith.js
@@ -1,19 +1,315 @@
 import React from "react";
 import css from "styled-components";
+import { BsFileEarmarkPdf } from "react-icons/bs";
 
-const Header = css.div`
+const HadithContainer = css.div`
 
     display:flex;
+    flex-direction:column;
     justify-content:center;
     align-items:center;
-    font-size:4rem;
-    background:cyan;
+    width:100%;
 `;
+
+const Heading = css.h1`
+    // border:2px solid black;
+    // background:black;
+    color:black;   
+    margin:30px;
+
+`;
+
 function Hadith() {
   return (
-    <Header>
-      Website is under super vision due to some technical issue........
-    </Header>
+    <HadithContainer>
+      <h2>yet under construction...........</h2>
+      <Heading>Sahihain</Heading>
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/bukhari.webp"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sahih Al Bukhari(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/Muslim.jpg"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sahih Al Muslim(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2>Sunan-e-Arbah</h2>
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/Nasai.webp"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sunan Nasai(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/tirmzi.png"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sunan Tirmidhi(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/ibnedawood.jpg"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sunan Ibne Dawood(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card mb-3" style={{ "max-width": "50%" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src="/ibnemaja.webp"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Sunan Ibne Maja(Arabic+ Urdu)</h5>
+              <div>
+                <div>
+                  <div>
+                    <a href="">Download Jild 1</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 2</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+
+                  <div>
+                    <a href="">Download Jild 3</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 4</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 5</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                  <div>
+                    <a href="">Download Jild 6</a>
+                    <BsFileEarmarkPdf />
+                  </div>
+                </div>
+              </div>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HadithContainer>
   );
 }
 
